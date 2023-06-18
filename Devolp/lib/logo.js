37db0f035518c3188baf9784f4c74bff4d83e507
Haseebz1 <haseebkandore@hotmail.com>
2023-06-18 12:15:14 +0100

    fixed syntax error

--- Devolp/lib/logo.js
+++ Devolp/lib/logo.js
@@ -3,19 +3,22 @@ class Logo {
         this.logoText = "";
         this.logoShape = "";    
     }
-}
+
 
 setText(text, color) {
     if (text.length > 3) {
       throw new Error("Text must not exceed 3 characters.");
     }
     this.logoText = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
-  }
+  };
 
   setShape(shape) {
     this.logoShape = shape.render();
-  }
+  };
 
   render() {
     return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.logoShape}${this.logoText}</svg>`;
   }
+  
+}
+
