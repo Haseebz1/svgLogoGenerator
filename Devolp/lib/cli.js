02726b2a6bf76245efa60c73afef48e7ef336c59
Haseebz1 <haseebkandore@hotmail.com>
2023-06-18 12:28:50 +0100

    questions added for user

--- Devolp/lib/cli.js
+++ Devolp/lib/cli.js
@@ -2,3 +2,31 @@ const inquirer = require('inquirer');
 const {WriteFile} = require('fs/promises');
 const Logo = require("./logo");
 const { Circle, Triangle, Square } = require("./shapes");
+
+let userQuestions = [
+    {
+      name: "text",
+      type: "input",
+      message:
+        "Enter text for the logo. (Must not be more than 3 characters.)",
+      validate: (text) =>
+        text.length <= 3 ||
+        "The message must not contain more than 3 characters",
+    },
+    {
+      name: "textColor",
+      type: "input",
+      message: "Enter a text color",
+    },
+    {
+      name: "shapeType",
+      type: "list",
+      message: "Select a shape for the logo",
+      choices: ["circle", "square", "triangle"],
+    },
+    {
+      name: "shapeColor",
+      type: "input",
+      message: "Enter a shape color",
+    },
+  ];
