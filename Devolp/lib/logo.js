affc4eeaf287191c508d31f4310b41c1db5cc23e
Haseebz1 <haseebkandore@hotmail.com>
2023-06-18 12:14:01 +0100

    render method in class logo

--- Devolp/lib/logo.js
+++ Devolp/lib/logo.js
@@ -15,3 +15,7 @@ setText(text, color) {
   setShape(shape) {
     this.logoShape = shape.render();
   }
+
+  render() {
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.logoShape}${this.logoText}</svg>`;
+  }
