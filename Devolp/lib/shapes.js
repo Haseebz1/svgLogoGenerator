bebe56ca5c4d797c620ae937d1aa3e32992fe394
Haseebz1 <haseebkandore@hotmail.com>
2023-06-18 11:49:17 +0100

    created square class inhertied from shape

--- Devolp/lib/shapes.js
+++ Devolp/lib/shapes.js
@@ -1,9 +1,15 @@
 class Shape {
-    constructor (){
-        this.color = "";
-    }
+  constructor() {
+    this.color = "";
+  }
 
-    setColor(color){
-        this.color = color;
-    }
+  setColor(color) {
+    this.color = color;
+  }
+}
+
+class Square extends Shape {
+  render() {
+    return `<rect x="90" y="40" width="120" height="120" fill="${this.color}" />`;
+  }
 }
